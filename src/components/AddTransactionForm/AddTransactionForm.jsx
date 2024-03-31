556f03b094eca2485561510ffe56c1301e95e0e0
Dan Retegan <danretegan@yahoo.com>
2024-03-31 07:50:20 +0300

    add API send data

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -6,7 +6,7 @@ const AddTransactionForm = () => {
   const [amount, setAmount] = useState('');
   const [date, setDate] = useState('');
   const [comment, setComment] = useState('');
-  const [category, setCategory] = useState(expensesCategories[0]); // Starea pentru categoria de cheltuială, inițializată cu prima categorie din listă
+  const [category, setCategory] = useState(expensesCategories[0].id); // Inițializăm cu id-ul primei categorii din listă
 
   const handleAmountChange = event => {
     setAmount(event.target.value);
@@ -24,19 +24,42 @@ const AddTransactionForm = () => {
     setCategory(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault();
-    // Aici vom trimite datele la un API
-    console.log('Tip tranzacție:', transactionType);
-    console.log('Sumă:', amount);
-    console.log('Dată:', date);
-    console.log('Comentariu:', comment);
-    console.log('Categorie:', category);
-    // După trimiterea datelor resetam formularul:
-    setAmount('');
-    setDate('');
-    setComment('');
-    setCategory(expensesCategories[0]); // Se resetează categoria la prima din listă
+
+    const transactionData = {
+      transactionDate: date,
+      type: transactionType.toUpperCase(),
+      categoryId: category, // Utilizăm id-ul categoriei
+      comment: comment,
+      amount: parseFloat(amount),
+    };
+
+    try {
+      const response = await fetch('/api/transactions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Adaugă header-ul de autorizare aici, dacă este necesar
+        },
+        body: JSON.stringify(transactionData),
+      });
+
+      if (response.ok) {
+        // Tranzacția a fost creată cu succes:
+        console.log('Tranzacția a fost creată cu succes');
+        // Resetăm câmpurile formularului:
+        setAmount('');
+        setDate('');
+        setComment('');
+        setCategory(expensesCategories[0].id); // Resetăm la id-ul primei categorii din listă
+      } else {
+        // A apărut o eroare la crearea tranzacției
+        console.error('Eroare la crearea tranzacției:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Eroare la crearea tranzacției:', error);
+    }
   };
 
   const handleToggleTransactionType = () => {
@@ -80,8 +103,8 @@ const AddTransactionForm = () => {
             onChange={handleCategoryChange}
           >
             {expensesCategories.map(category => (
-              <option key={category} value={category}>
-                {category}
+              <option key={category.id} value={category.id}>
+                {category.name}
               </option>
             ))}
           </select>
