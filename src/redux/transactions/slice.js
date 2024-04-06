930c303437a8568e3ec9801eaa25f4de92e84e0e
Dan Retegan <danretegan@yahoo.com>
2024-04-06 09:28:13 +0300

    transactions slice update

--- src/redux/transactions/slice.js
+++ src/redux/transactions/slice.js
@@ -1,14 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import {
-//   addTransactionThunk,
-//   getAllTransactionsThunk,
-//   updateTransactionThunk,
-//   deleteTransactionThunk,
-//   getTransactionCategoriesThunk,
-//   getTransactionSummaryThunk,
-// } from './operations';
 
-import { addTransaction, fetchAllTransactions } from './operations';
+import {
+  addTransaction,
+  fetchAllTransactions,
+  updateTransactionThunk,
+  deleteTransactionThunk,
+  getTransactionCategoriesThunk,
+  getTransactionSummaryThunk,
+} from './operations';
 
 const initialState = {
   categories: [],
@@ -55,97 +54,65 @@ const transactionsSlice = createSlice({
         state.isLoading = false;
         state.error = null;
         state.items = action.payload;
+      })
+
+      // * updateTransactionThunk
+      .addCase(updateTransactionThunk.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(updateTransactionThunk.fulfilled, (state, action) => {
+        const index = state.items.findIndex(t => t.id === action.payload.id);
+        if (index !== -1) {
+          state.items[index] = action.payload;
+        }
+        state.isLoading = false;
+      })
+      .addCase(updateTransactionThunk.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      // * deleteTransactionThunk
+      .addCase(deleteTransactionThunk.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(deleteTransactionThunk.fulfilled, (state, action) => {
+        state.items = state.items.filter(t => t.id !== action.payload);
+        state.isLoading = false;
+      })
+      .addCase(deleteTransactionThunk.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      // * getTransactionCategoriesThunk
+      .addCase(getTransactionCategoriesThunk.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getTransactionCategoriesThunk.fulfilled, (state, action) => {
+        state.categories = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(getTransactionCategoriesThunk.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+
+      // * getTransactionSummaryThunk
+      .addCase(getTransactionSummaryThunk.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getTransactionSummaryThunk.fulfilled, state => {
+        state.isLoading = false;
+      })
+      .addCase(getTransactionSummaryThunk.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
       });
-
-    // todo: de continuat de aici in jos
-
-    // PENDING:
-    // .addCase(addTransactionThunk.pending, (state, action) => {
-    //   state.loading = true;
-    //   state.error = null;
-    // })
-    // .addCase(getAllTransactionsThunk.pending, (state, action) => {
-    //   state.loading = true;
-    //   state.error = null;
-    // })
-    // .addCase(updateTransactionThunk.pending, (state, action) => {
-    //   state.loading = true;
-    //   state.error = null;
-    // })
-    // .addCase(deleteTransactionThunk.pending, (state, action) => {
-    //   state.loading = true;
-    //   state.error = null;
-    // })
-    // .addCase(getTransactionCategoriesThunk.pending, (state, action) => {
-    //   state.loading = true;
-    //   state.error = null;
-    // })
-    // .addCase(getTransactionSummaryThunk.pending, (state, action) => {
-    //   state.loading = true;
-    //   state.error = null;
-    // })
-
-    // FULFILLED:
-    // .addCase(addTransactionThunk.fulfilled, (state, action) => {
-    //   state.transactions.push(action.payload);
-    //   state.loading = false;
-    // })
-    // .addCase(getAllTransactionsThunk.fulfilled, (state, action) => {
-    //   state.transactions = action.payload;
-    //   state.loading = false;
-    // })
-    // .addCase(updateTransactionThunk.fulfilled, (state, action) => {
-    //   const index = state.transactions.findIndex(
-    //     t => t.id === action.payload.id
-    //   );
-    //   if (index !== -1) {
-    //     state.transactions[index] = action.payload;
-    //   }
-    //   state.loading = false;
-    // })
-    // .addCase(deleteTransactionThunk.fulfilled, (state, action) => {
-    //   state.transactions = state.transactions.filter(
-    //     t => t.id !== action.payload
-    //   );
-    //   state.loading = false;
-    // })
-    // .addCase(getTransactionCategoriesThunk.fulfilled, (state, action) => {
-    //   state.categories = action.payload;
-    //   state.loading = false;
-    // })
-    // .addCase(getTransactionSummaryThunk.fulfilled, (state, action) => {
-    //   // Handle fulfillment of transaction summary thunk
-    //   // Modify state accordingly
-    //   state.loading = false;
-    // })
-
-    // REJECTED:
-    // .addCase(addTransactionThunk.rejected, (state, action) => {
-    //   state.loading = false;
-    //   state.error = action.payload;
-    // })
-    // .addCase(getAllTransactionsThunk.rejected, (state, action) => {
-    //   state.loading = false;
-    //   state.error = action.payload;
-    // })
-    // .addCase(updateTransactionThunk.rejected, (state, action) => {
-    //   state.loading = false;
-    //   state.error = action.payload;
-    // })
-    // .addCase(deleteTransactionThunk.rejected, (state, action) => {
-    //   state.loading = false;
-    //   state.error = action.payload;
-    // })
-    // .addCase(getTransactionCategoriesThunk.rejected, (state, action) => {
-    //   state.loading = false;
-    //   state.error = action.payload;
-    // })
-    // .addCase(getTransactionSummaryThunk.rejected, (state, action) => {
-    //   // Handle rejection of transaction summary thunk
-    //   // Modify state accordingly
-    //   state.loading = false;
-    //   state.error = action.payload;
-    // });
   },
 });
 
