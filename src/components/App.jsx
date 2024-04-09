2f2b103f5f5a9b830fbc6138668974d3170cd205
RaresDrg <dragan_rares13@yahoo.com>
2024-04-09 12:59:25 +0300

    Project done
    Project done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
-import Notify from './common/Notify/Notify';
+import { lazy } from 'react';
 
-import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 import SharedLayout from './SharedLayout/SharedLayout';
+import Notify from './common/Notify/Notify';
 
 import RestrictedLoginPage from 'pages/LoginPage/RestrictedLoginPage';
 import RestrictedRegisterPage from 'pages/RegisterPage/RestrictedRegisterPage';
 
-import RestrictedHomePage from 'pages/HomePage/RestrictedHomePage';
-import RestrictedStatisticsPage from 'pages/StatisticsPage/RestrictedStatisticsPage';
-import RestrictedCurrencyPage from 'pages/CurrencyPage/RestrictedCurrencyPage';
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+const HomePage = lazy(() => import('pages/HomePage/RestrictedHomePage'));
+const StatisticsPage = lazy(() =>
+  import('pages/StatisticsPage/RestrictedStatisticsPage')
+);
+const CurrencyPage = lazy(() =>
+  import('pages/CurrencyPage/RestrictedCurrencyPage')
+);
 
 const App = () => {
   const isOnMobile = useMediaQuery({ query: '(max-width: 768px)' });
@@ -22,11 +27,9 @@ const App = () => {
         <Route path="/register" element={<RestrictedRegisterPage />} />
 
         <Route path="/dashboard" element={<SharedLayout />}>
-          <Route index element={<RestrictedHomePage />} />
-          <Route path="statistics" element={<RestrictedStatisticsPage />} />
-          {isOnMobile && (
-            <Route path="currency" element={<RestrictedCurrencyPage />} />
-          )}
+          <Route index element={<HomePage />} />
+          <Route path="statistics" element={<StatisticsPage />} />
+          {isOnMobile && <Route path="currency" element={<CurrencyPage />} />}
         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
